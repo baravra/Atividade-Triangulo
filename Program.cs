9343334614b23b27b407c2b6c4ddddc7fbf67f6e
baravra <barbaraleao0606@gmail.com>
2023-03-13 18:17:02 -0300

    ADD FILES

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Atividade4___Triangulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.WriteLine("Insira o valor de um dos lados");
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Insira o valor de um dos lados");
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Insira o valor de um dos lados");
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (a < 0 || b < 0 || c < 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Nenhum dos valores deve ser negativo!");
+                return;
+            }
+
+            Triangulo t = new Triangulo();
+            t.a = a;
+            t.b = b;
+            t.c = c;
+
+            t.ValidarTriangulo();
+
+        }
+    }
+}
---
+++ Triangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4___Triangulos
+{
+    public class Triangulo
+    {
+        public double a, b, c;
+
+        public void ValidarTriangulo()
+        {
+            if(a == b && a == c)
+            {
+                Console.WriteLine("Triango EQUILATERO");
+            }
+            else if ((a == b && a != c) || (a == c && a != b) || (b == c && b != a) )
+            {
+                Console.WriteLine("Triango ISOSCELES");
+            }
+            else if ( a >= (b+c) )
+            {
+                Console.WriteLine("NAO FORMA TRIANGULO");
+            }
+            else if ( (a * a) == ( (b*b) + (c*c) ) )
+            {
+                Console.WriteLine("Triango RETANGULO");
+            }
+            else if ((a * a) > ((b * b) + (c * c)))
+            {
+                Console.WriteLine("Triango OBTUSANGULO");
+            }
+            else if ((a * a) < ((b * b) + (c * c)))
+            {
+                Console.WriteLine("Triango ACUTANGULO");
+            }
+        }
+    }
+}
